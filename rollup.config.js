59c0d6ba87711e33a96e9fccdd02b85b14126870
jailgreen <36865973+jailgreen@users.noreply.github.com>
2018-05-10 16:43:46 +0300

    State before rollup-css

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,7 @@
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
-import postcss from 'rollup-plugin-postcss';
-
-// PostCSS plugins
-import simplevars from 'postcss-simple-vars';
-import nested from 'postcss-nested';
-import cssnext from 'postcss-cssnext';
-import cssnano from 'cssnano';
 
 /* 
  * @license    https://opensource.org/licenses/BSD-3-Clause New BSD License
@@ -23,27 +15,13 @@ export default {
     file: 'public/js/main.min.js',
     format: 'iife',
     name: 'App',
-    sourcemap: 'inline',
+    sourcemap: 'true',
     globals: {
       jquery: 'jQuery'
     }
   },
   plugins: [
-    postcss({
-      plugins: [
-        simplevars(),
-        nested(),
-        cssnext({ warnForDuplicates: false }),
-        // cssnano(),
-      ],
-      extensions: ['.css'],
-    }),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
+    resolve(),
     eslint({
       exclude: [
         'resources/scss/**',
